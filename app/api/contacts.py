5b303f07aab27fef995a8154e0851f591bd1a340
Yelyzaveta Sheremet <elizabethsheremet@gmail.com>
2026-06-14 10:36:01 -0400

    Add current user dependency to contact-related endpoints for user-specific access

--- app/api/contacts.py
+++ app/api/contacts.py
@@ -4,6 +4,8 @@ from app.db.db_connection import get_db
 from app.repository.contacts import ContactRepository
 from app.services.contacts import ContactService
 from app.schemas.contacts import ContactModel, ContactResponse
+from app.db.models import User
+from app.services.auth import get_current_user
 
 router = APIRouter(prefix="/contacts", tags=["contacts"])
 
@@ -21,31 +23,35 @@ async def get_contacts(
     skip: int = 0,
     limit: int = 10,
     service: ContactService = Depends(get_service),
+    current_user: User = Depends(get_current_user)
 ):
-    return await service.get_contacts(first_name, last_name, email, skip, limit)
+    return await service.get_contacts(first_name, last_name, email, skip, limit, current_user)
 
 
 @router.get("/birthdays", response_model=list[ContactResponse])
 async def get_birthdays(
     service: ContactService = Depends(get_service),
+    current_user: User = Depends(get_current_user)
 ):
-    return await service.get_b_days()
+    return await service.get_b_days(current_user)
 
 
 @router.get("/{contact_id}", response_model=ContactResponse)
 async def get_contact(
     contact_id: int,
     service: ContactService = Depends(get_service),
+    current_user: User = Depends(get_current_user)
 ):
-    return await service.get_contact(contact_id)
+    return await service.get_contact(contact_id, current_user)
 
 
 @router.post("/", response_model=ContactResponse, status_code=201)
 async def create_contact(
     body: ContactModel,
     service: ContactService = Depends(get_service),
+    current_user: User = Depends(get_current_user)
 ):
-    return await service.create_contact(body)
+    return await service.create_contact(body, current_user)
 
 
 @router.put("/{contact_id}", response_model=ContactResponse)
@@ -53,13 +59,15 @@ async def update_contact(
     contact_id: int,
     body: ContactModel,
     service: ContactService = Depends(get_service),
+    current_user: User = Depends(get_current_user)
 ):
-    return await service.update_contact(contact_id, body)
+    return await service.update_contact(contact_id, body, current_user)
 
 
 @router.delete("/{contact_id}", status_code=204)
 async def delete_contact(
     contact_id: int,
     service: ContactService = Depends(get_service),
+    current_user: User = Depends(get_current_user)
 ):
-    await service.delete_contact(contact_id)
+    await service.delete_contact(contact_id, current_user)
